cd62152a2ad3a372382915bdcbe59ee6c4da956b
PabloPavla <pablo.garciapalacios@a.vedrunasevillasj.es>
2025-06-05 18:47:56 +0200

    Cambio 2

--- src/main/java/com/javafx/daw/modificador.java
+++ src/main/java/com/javafx/daw/modificador.java
@@ -13,5 +13,16 @@ public class modificador {
         return character;
     }
 
+    public int cambioletra(char character){
+
+        char codigoASCII = character;
+
+        int num = codigoASCII;
+
+        System.out.println("EL caracter correspondiente es: "+character);
+        System.out.println("El numero introducido es: "+num);
+        
+        return num;
+    }
 
 }
